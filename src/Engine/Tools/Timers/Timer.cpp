61d944ac5cc6119da27f1202afd6c6be198e649d
gparent <gparent@c8be0275-da4d-0410-8729-c19fdb78b3e3>
2008-07-08 19:58:40 +0000

    Slightly better code for the timer.

--- src/Engine/Tools/Timers/Timer.cpp
+++ src/Engine/Tools/Timers/Timer.cpp
@@ -4,9 +4,10 @@
 * are made available under the terms of the GNU Lesser Public License v2.1
 * which accompanies this distribution, and is available at
 * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-* 
+*
 * Contributors:
 *     Jonathan 'Bladezor' Bastnagel - initial implementation and documentation
+*     Guillaume 'P90' Parent - minor performance improvements
 *****************************************************************************************/
 #include "Timer.h"
 #include <stdlib.h>
@@ -23,6 +24,23 @@ OE::Tools::Timers::Timer::Timer()
 #endif  
 	_dStartTime = 0;
 	_dEndTime = 0;
+}
+
+OE::Tools::Timers::Timer::Timer(const TIME &t)
+{
+#ifdef WIN32
+    QueryPerformanceFrequency(&Freq);
+
+    QueryPerformanceCounter(&StartCount);
+    _dStartTime = StartCount.QuadPart * (1000000.0 / Freq.QuadPart);
+    StartCount.QuadPart = static_cast<LONGLONG>((_dStartTime - t.Microseconds) / (1000000.0 / Freq.QuadPart));
+    _dStartTime = StartCount.QuadPart * (1000000.0 / Freq.QuadPart);
+#else
+    gettimeofday(&StartCount, NULL);
+    StartCount.tv_sec += t.Seconds; // Set start time at CURRENT - TIMER TIME. Otherwise our timer's useless.
+    StartCount.tv_usec += t.Microseconds;
+    _dStartTime = (StartCount.tv_sec * 1000000.0) + StartCount.tv_usec;
+#endif
 }
 
 OE::Tools::Timers::Timer::~Timer()
@@ -80,14 +98,20 @@ double OE::Tools::Timers::Timer::GetElapsedSec()
 
 OE::Tools::Timers::TIME OE::Tools::Timers::Timer::GetElapsed()
 {
-	TIME temp;
-	temp.Microseconds = GetElapsedMicroSec();
-	temp.Milliseconds = GetElapsedMilliSec();
-	temp.Seconds = GetElapsedSec();
-	temp.Minutes = GetElapsedSec() * 0.01666666;
-	temp.Hours = temp.Minutes * 0.01666666;
-	temp.Days = temp.Hours * 0.04166666;
-	temp.Years = temp.Days * 0.00277777;
+	return buildTimeStruct(GetElapsedMicroSec());
+}
 
-	return temp;
+// This function builds a TIME struct using its smallest element (microseconds) and returns it.
+OE::Tools::Timers::TIME OE::Tools::Timers::buildTimeStruct(double Microseconds)
+{
+    TIME tBuilt;
+    tBuilt.Microseconds = Microseconds;
+    tBuilt.Milliseconds = tBuilt.Microseconds * 0.001;
+    tBuilt.Seconds = tBuilt.Milliseconds * 0.001;
+    tBuilt.Minutes = tBuilt.Seconds * 0.01666666;
+    tBuilt.Hours = tBuilt.Minutes * 0.01666666;
+    tBuilt.Days = tBuilt.Hours * 0.04166666;
+    tBuilt.Years = tBuilt.Days * 0.00277777;
+
+    return tBuilt;
 }
